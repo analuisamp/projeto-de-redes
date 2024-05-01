01fd16401eac7d53e800c19d16b3261e3badbccb
analuisamp <dev.06@controlit.com.br>
2024-04-30 23:09:35 -0300

    adding server functionalitys

--- servidor.py
+++ servidor.py
@@ -1,10 +1,12 @@
 import socket
+import datetime
 
 def main():
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     server_socket.bind(('localhost', 5000))
 
     print("Server is running...")
+    response_count = 0  # Inicializa response_count aqui
 
     while True:
         receive_buffer = bytearray(1024)
@@ -18,14 +20,18 @@ def main():
             print("Opção 4 escolhida")
             break  # Encerra o loop para fechar o servidor
         elif client_message == '1':
-            server_message = "Data e hora atual"
-            print("Data e hora atual")
+            response_count += 1
+            current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            server_message = f"Data e hora atual: {current_time}"
+            print("Data e hora atual:", current_time)
         elif client_message == '2':
-            server_message = "Mensagem motivacional para o fim do semestre"
+            response_count += 1
+            server_message = "Lembre-se de manter o foco, a determinação e a perseverança.\n"
             print("Mensagem motivacional para o fim do semestre")
         elif client_message == '3':
-            server_message = "Quantidade de respostas emitidas pelo servidor"
-            print("Quantidade de respostas emitidas pelo servidor")
+            response_count += 1  # Incrementa response_count
+            server_message = f"Quantidade de respostas emitidas pelo servidor: {response_count}"
+            print("Quantidade de respostas emitidas pelo servidor:", response_count)
 
         send_buffer = server_message.encode()
         server_socket.sendto(send_buffer, client_address)  # Envia a resposta para o cliente
@@ -34,3 +40,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
